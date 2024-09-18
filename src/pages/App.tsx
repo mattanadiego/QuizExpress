143fac1a7255387f40313b4050d40f49213b1593
Diego Mattana <mattanadiego@gmailcom>
2024-09-18 03:20:23 -0300

    Commit inicial. Configuración inicial del proyecto.

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import logo from '../assets/images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> Welcome to </p>
+        <p> Quiz Express! </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
